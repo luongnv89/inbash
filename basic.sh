a5da6dc565b7f7695f37f6adc5c251eb54c38893
Montimage <montimage@Montimages-MacBook-Pro.local>
2016-08-18 10:58:46 +0200

    deleted:    basic.sh 	deleted:    c_cpp.sh 	deleted:    gxx4.9.sh 	deleted:    interfaces 	deleted:    mount-share.sh 	deleted:    nodejs.sh 	deleted:    php-mongodb-extension.sh 	deleted:    show_ip 	deleted:    show_ip_login.sh 	deleted:    tomahawk.sh 	deleted:    update_network_interfaces.sh 	deleted:    valgrind.sh

--- basic.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo '[Info] Going to install some basic packages: vim, git, wget'
-sudo apt-get update -y
-sudo apt-get install -y git vim wget
--- update_network_interfaces.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Replace default interfaces file with new interfaces file. To update more network interface, please look at the file : interfaces
-sudo cp interfaces /etc/network/interfaces
-
-# Bring eth1 up
-sudo ifup eth1
-sudo /sbin/ifconfig
-
-# Bring more network interface up here
