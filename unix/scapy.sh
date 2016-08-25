e7de1dcf255b5e3f4ef6fbfccb25f987f824ec82
Luong NGUYEN <luongnv89@gmail.com>
2016-08-25 18:11:00 +0200

    script for installing scapy

--- unix/scapy.sh
+++ unix/scapy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Get started with scapy: http://www.secdev.org/projects/scapy/demo.html
-apt-get install -y python python-pip
+apt-get update
+apt-get install -y software-properties-common build-essential tcpdump gnuplot
+apt-get install -y python python-pip python-numpy python-pyx python-crypto python-gnuplot
 pip install scapy
