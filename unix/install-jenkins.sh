ba4fb4defbfa7e8c5fe5d6483cc8496fdf900f76
Luong Nguyen <luongnv89@gmail.com>
2017-06-25 00:31:28 +0200

    Add installation for jenkins

--- unix/install-jenkins.sh
+++ unix/install-jenkins.sh
@@ -4,10 +4,20 @@
 sudo apt-get update -y
 
 # Install java
-sudo add-apt-repository ppa:webupd8team/java -y
-sudo apt-get update -y
-sudo apt-get install oracle-java8-installer
+# sudo add-apt-repository ppa:webupd8team/java -y
+# sudo apt-get update -y
+# sudo apt-get install oracle-java8-installer
+
+sudo apt-get install -y openjdk-8-jre
+sudo apt-get install -y openjdk-8-jdk
+
 
 # Install jenkins
-wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war
-java -jar jenkins.war
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo echo 'deb https://pkg.jenkins.io/debian-stable binary/' >> /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install jenkins
+
+# Alternative
+# wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war
+# java -jar jenkins.war
