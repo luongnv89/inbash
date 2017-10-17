780f1ccf51cb411dcfd59989c8d23e026e73843c
Luong NGUYEN <luongnv89@gmail.com>
2017-10-17 15:25:24 +0200

    Update installing development tools

--- redhat/c_cpp.sh
+++ redhat/c_cpp.sh
@@ -5,6 +5,4 @@ echo '[info] Install some package for developing c/c++'
 
 yum update
 # C/C++ environment
-yum install -y build-essential gcc g++ cmake make gdb
-yum autoremove
-yum autoclean
+yum group install "Development Tools"
