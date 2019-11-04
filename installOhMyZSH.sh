96886448a3f8917bc58b6447e689539b7e60016e
Luong NGUYEN <luongnv89@gmail.com>
2019-11-04 11:44:32 +0100

    Add install zsh command

--- installOhMyZSH.sh
+++ installOhMyZSH.sh
@@ -1,3 +1,5 @@
+# If linux:
+sudo apt-get install zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 mv zshrc-config ~/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
