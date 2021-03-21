a429ee1e01199b5c79edaff2834cb0c242ae04d7
Luong NGUYEN <luongnv89@gmail.com>
2021-03-21 14:31:09 +0100

    do not remove zsh config file

--- setup-ohMyZsh.sh
+++ setup-ohMyZsh.sh
@@ -2,4 +2,4 @@
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
-mv zshrc-config ~/.zshrc
+cp zshrc-config ~/.zshrc
