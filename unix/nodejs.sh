72c6406e28e21292f129c4b214aae4afa7860af6
Luong NGUYEN <luongnv89@gmail.com>
2021-03-21 14:51:32 +0100

    update nodejs version 15

--- unix/nodejs.sh
+++ unix/nodejs.sh
@@ -4,7 +4,7 @@ echo '[info] Install some package for developing Node js'
 
 apt-get update
 # Using Ubuntu
-curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 echo $(node -v)
